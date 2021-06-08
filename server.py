a7c456f63b4fc17aca62700cbc41b25c0f31f205
Sandra You <syou327@yahoo.com>
2021-06-08 05:38:21 +0000

    Added changes to signup and login

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ import crud
 
 
 app = Flask(__name__)
-app.secret_key = 'dev'
+app.secret_key = '89qhfhje&*djfka8238420*2i#6'
 app.jinja_env.undefined = StrictUndefined
 
 
@@ -18,18 +18,9 @@ def homepage():
     return render_template('index.html')
 
 
-@app.route('/users')
-def all_users():
-    """Show all users."""
-
-    users = crud.get_users()
-
-    return render_template('all_users.html', users=users)
-
-
-@app.route('/users', methods=['POST'])
-def register_user():
-    """Create a new user."""
+@app.route('/signup', methods=['POST'])
+def create_user():
+    """Register a new user."""
 
     full_name = request.form.get('full_name')
     email = request.form.get('email')
@@ -40,46 +31,45 @@ def register_user():
     if user:
         flash('Email already exist. Please log in or try again.')
     else:
-        # crud.create_user(full_name, phone_number, email, password)
         crud.create_user(full_name, email, password)
-        flash('Account has been successfully created. Welcome! Please log in.')
+        flash('Account has been successfully created. Please sign in.')
 
     return redirect('/') # redirect back to homepage
 
 
-@app.route('/user/<user_id>')
-def show_user(user_id):
-    """Display user's details"""
 
-    user = crud.get_user_by_id(user_id)
+@app.route('/signin', methods=['POST'])
+def process_login():
+    """Log user in."""
 
-    return render_template('user_details.html', user=user)
-
-
-@app.route('/login', methods=['POST'])
-def login_process():
-    """Process login."""
     email = request.form["email"]
     password = request.form["password"]
 
     user = crud.get_user_by_email(email)
 
-    if user:
-        flash('You have successfully logged in.')
-    else:
-        crud.create_user(full_name, email, password)
-        flash('Email does not exist. Please try again or sign up.')
+    if not user:
+        flash("Email does not exist. Please try again.")
+        return redirect('/')
+
+    if user.password != password:
+        flash("Oops. Password is invalid. Please try again.")
+        return redirect('/')
+
+    session["logged_user_in"] = user.email
+    flash("You have logged in succesfully.")
 
     return redirect('/welcome')
 
 
-@app.route('/logout')
+@app.route('/signout')
 def logout():
-    """Process log out"""
+    """Log user out."""
 
-    #TODO
+    del session["logged_user_in"]
 
-    return redirect("/login")
+    flash("Logged out successfully.")
+
+    return redirect("/")
 
 
 @app.route('/welcome')
@@ -87,6 +77,24 @@ def welcome():
     """Show the welcome page."""
 
     return render_template('welcome.html')
+
+
+# @app.route('/users')
+# def all_users():
+#     """Show all users."""
+
+#     users = crud.get_users()
+
+#     return render_template('all_users.html', users=users)
+
+
+# @app.route('/user/<user_id>')
+# def show_user(user_id):
+#     """Display user's details"""
+
+#     user = crud.get_user_by_id(user_id)
+
+#     return render_template('user_details.html', user=user)
 
 
 if __name__ == '__main__':
